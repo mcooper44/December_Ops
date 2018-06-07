cde89e1471df3efb9337c17b38b98fd46bd21648
matt <m.cooper44@gmail.com>
2018-06-07 01:23:11 +0000

    cleaning up syntax errors

--- ops_route_creator.py
+++ ops_route_creator.py
@@ -1,6 +1,10 @@
 import sqlite3
 from basket_sorting_Geocodes import Delivery_Household
 from basket_sorting_Geocodes import Delivery_Routes
+from collections import namedtuple
+from address_parser_and_geocoder import Coordinates  
+from address_parser_and_geocoder import SQLdatabase
+from address_parser_and_geocoder import AddressParser
 
 Client = namedtuple('Client', 'size location')
 Geolocation = namedtuple('Geolocation', 'lat long')
@@ -31,7 +35,7 @@ export_file = Export_File_Parser('2018source.csv',fnames.ID) # I open a csv
     # for testing us test_export.csv
 export_file.open_file()
 
-2018routes = Delivery_Routes(7, 1) 
+a2018routes = Delivery_Routes(7, 1) 
 
 
 hh_dict = {}
@@ -40,18 +44,18 @@ for line in export_file: # I am a csv object
     line_object = Visit_Line_Object(line,fnames.ID)
     address, city, _ = line_object.get_address()
     applicant = line_object.get_applicant_ID()
-    family size = line_object.visit_household_Size
+    family_size = line_object.visit_household_Size
 
     simple_address, _ = address_parser.parse(address, applicant)
     lt, lg =  coordinate_manager.get_coordinates(simple_address, city)   
     if all(lt, lg):
         hh_dict[file_id] = Delivery_Household(applicant, None, family_size, lt, lg)
 
-2018routes.get_status()
-2018routes.get_route_collection(hh_dict))
-2018routes.sort_method()
-2018routes.create_route_db('2018.db')
-2018routes.log_route_in_db()
+a2018routes.get_status()
+a2018routes.get_route_collection(hh_dict)
+a2018routes.sort_method()
+a2018routes.create_route_db('2018.db')
+a2018routes.log_route_in_db()
 
 
 
