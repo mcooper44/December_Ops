c9feb94ae533697de5215cdf409fca984e8903f6
matt <m.cooper44@gmail.com>
2018-07-13 17:10:28 +0000

    added calls to insert summary cards

--- delivery_card_creator.py
+++ delivery_card_creator.py
@@ -5,46 +5,46 @@ slips for printing.
 import xlsxwriter
 #http://xlsxwriter.readthedocs.io/format.html
 
-# HELPER FUNCTIONS
-
-def diet_parser(diet_string):
-    '''
-    this function returns the string of redundant dietary conditions
-    minus the redundant conditions
-    '''
-    diet = ''
-    diet_conditions = set(diet_string.split(',')) # a unique list of conditions split on the commas
-    if 'Other' in diet_conditions:
-        diet_conditions.remove('Other') # we don't want this because it has zero information
-    
-    for condition in diet_conditions:
-        if condition != '': # if it's not blank
-            diet += ', '.join(condition)
-    return diet
 
 class Delivery_Slips():
+    '''
+    This is a monster, but it works? 
+    Initialize key points of information and formatting
+    Then cycle through the sorted routes and insert them into the 
+    correct cells, then move them down, and adjust for the weirdness
+    that happens with page transitions.
+
+    '''
     def __init__(self, book_name):
         self.workbook = xlsxwriter.Workbook(book_name)
         self.worksheet = None
+        self.cell_format_size = None
         self.off_set = 14
         self.spot_counter = 0
-        self.l_n = [0,1,2,3,4,5,6,7,8,9,10]
+        self.l_n = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
         if self.workbook:
             self.worksheet = self.workbook.add_worksheet('food')
             self.worksheet.set_margins(0.25, 0.25, 0.15, 0.15)
-        
+            self.worksheet.set_column(0, 0, 14.5) # col A
+            self.worksheet.set_column(2, 2, 16.5) # col C
+            self.worksheet.set_column(3, 3, 6) # col D
+            self.worksheet.set_column(8, 8, 4) # col I
+            self.worksheet.set_column(9, 9, 2) # col J
+            self.cell_format_size = self.workbook.add_format()
+            self.cell_format_size.set_font_size(8) # for writing the address list
+
     def add_household(self, route, summary):
         file_id, rn, rl = route
 
-        diet = diet_parser(summary.diet) # reformat diet to cut out redundancy
-        phone = str(summary.phone) # whatever is in the field - not the formatted number
+        diet = summary.diet
+        phone = summary.phone
         # cell locations 
         hh_str = 'A{}'.format(self.l_n[1]) # string hh size
         hh_size = 'B{}'.format(self.l_n[1]) # actual hh size
         f_name = 'C{}'.format(self.l_n[1]) # first name cell location
         l_name = 'D{}'.format(self.l_n[1]) # l name cell location
-        ph_num = 'A{}'.format(self.l_n[5]) # phone number cell location
+        ph_num = 'C{}'.format(self.l_n[5]) # phone number cell location
         address = 'C{}'.format(self.l_n[2]) # street address
         addressl2 = 'C{}'.format(self.l_n[3]) # line 2 unit number et al
         ID_num_str = 'A{}'.format(self.l_n[3]) # string of Request ID 
@@ -52,7 +52,7 @@ class Delivery_Slips():
         city = 'C{}'.format(self.l_n[4])
         postal_code = 'D{}'.format(self.l_n[4])
         diet_str = 'A{}'.format(self.l_n[6])
-        diet_actual = 'B{}'.format(self.l_n[5])
+        #diet_actual = 'B{}'.format(self.l_n[5])
         driver_str = 'A{}'.format(self.l_n[8])# driver name string
         route_str = 'H{}'.format(self.l_n[1]) # Route string
         route_num = 'I{}'.format(self.l_n[1]) # Actual route number
@@ -61,11 +61,9 @@ class Delivery_Slips():
 
         at_home_str = 'A{}'.format(self.l_n[9])
         at_yes_str = 'B{}'.format(self.l_n[9])
-        #at_no_str = 'C{}'.format(self.l_n[9])
         new_add_str = 'E{}'.format(self.l_n[9])
         left_hamper = 'A{}'.format(self.l_n[10])
         left_w_y_str = 'B{}'.format(self.l_n[10])
-        left_w_n_str = 'C{}'.format(self.l_n[10])
         left_with_str = 'E{}'.format(self.l_n[10])
         
         # write client info to worksheet
@@ -80,20 +78,18 @@ class Delivery_Slips():
         self.worksheet.write(city, summary.city) # city
         self.worksheet.write(postal_code, '_') #postal code
         self.worksheet.write(diet_str, 'SPECIAL DIET: {}'.format(diet))
-        self.worksheet.write(diet_actual, diet) # diet issues/preferences
+        #self.worksheet.write(diet_actual, diet) # diet issues/preferences
         self.worksheet.write(route_str, 'ROUTE:')
         self.worksheet.write(route_num, rn)
         self.worksheet.write(route_let, rl)
 
         # Driving related text strings
-        self.worksheet.write(driver_str, 'DRIVER NAME:_______________________________________________________________________')
+        self.worksheet.write(driver_str, 'DRIVER NAME(s): ATTEMPT 1_________________ ATTEMPT 2: _________________ ATTEMPT 3:_________________')
         self.worksheet.write(at_home_str, 'AT HOME:')
-        self.worksheet.write(at_yes_str, 'YES / NO ')
-        #self.worksheet.write(at_no_str, 'NO')
+        self.worksheet.write(at_yes_str, 'YES   /   NO ')
         self.worksheet.write(new_add_str, 'NEW ADDRESS: __________________________________')            
         self.worksheet.write(left_hamper, 'LEFT HAMPER:')
-        self.worksheet.write(left_w_y_str, 'YES / NO')
-        self.worksheet.write(left_w_n_str, 'NO')
+        self.worksheet.write(left_w_y_str, 'YES   /   NO')
         self.worksheet.write(left_with_str, 'WITH: __________________________________________')
 
         self.spot_counter += 1 # move the spot up one
@@ -111,6 +107,96 @@ class Delivery_Slips():
             self.l_n[8] += self.off_set
             self.l_n[9] += self.off_set
             self.l_n[10] += self.off_set
+            self.l_n[11] += self.off_set
+            self.l_n[12] += self.off_set
+            self.l_n[13] += self.off_set
+            self.l_n[14] += self.off_set
+            
+            self.spot_counter = 0 # start counting from zero
+            self.off_set += 4 # reset the offset and procede as normal
+            
+        else: # use the existing offset because we haven't reached the end of the page
+                        
+            self.l_n[1] += self.off_set
+            self.l_n[2] += self.off_set
+            self.l_n[3] += self.off_set
+            self.l_n[4] += self.off_set
+            self.l_n[5] += self.off_set
+            self.l_n[6] += self.off_set
+            self.l_n[7] += self.off_set
+            self.l_n[8] += self.off_set
+            self.l_n[9] += self.off_set
+            self.l_n[10] += self.off_set
+            self.l_n[11] += self.off_set
+            self.l_n[12] += self.off_set
+            self.l_n[13] += self.off_set
+            self.l_n[14] += self.off_set 
+    
+    def add_route_summary_card(self, summary):
+        '''
+        Adds a route summary card to the stack
+        call this method before writing an actual route card
+        to the stack
+        '''
+        rn = summary.route
+        box_count = summary.boxes
+        family_count = ['{} familys of {}'.format(box_count[x], x) for x in \
+                                  box_count]
+        street_set = summary.streets
+        hood = summary.neighbourhood
+        applicant_list = summary.applicant_list
+        size_counter = summary.sizes
+        letter_map = summary.letter_map # 'Box: A Family: 3 Diet: Halal
+        
+        # locations written as strings
+        rn_loc = 'A{}'.format(self.l_n[1]) # route number
+        #box_num_loc = 'G{}'.format(self.l_n[1]) # num of boxes One: 2, Two: 1
+        hood_loc = 'A{}'.format(self.l_n[2]) # neighbourhood
+        #num_hh_loc = 'C{}'.format(self.l_n[1]) # Number of familes
+        # locations with titles and separate string writes
+        street_title_loc = 'A{}'.format(self.l_n[3]) # STREETS: 
+
+        # write client info to worksheet
+        self.worksheet.write(rn_loc, 'Route: {}'.format(rn))
+        #self.worksheet.write(box_num_loc,'Family Sizes: {}'.format(family_count))
+        self.worksheet.write(hood_loc, 'Neighbourhood(s): {}'.format(str(hood)))
+        self.worksheet.write(street_title_loc, 'STREETS:')
+        
+        s_c = self.l_n[4]
+        for street in street_set:
+            self.worksheet.write('A{}'.format(s_c), street, self.cell_format_size)
+            s_c +=1
+        
+        lmap = self.l_n[4]
+        for hh_sum in letter_map:
+            self.worksheet.write('C{}'.format(lmap), letter_map[hh_sum])
+            lmap += 1
+        
+        b_f = self.l_n[4]
+        for size_bc in family_count:
+            self.worksheet.write('H{}'.format(b_f), '{}'.format(size_bc))
+            b_f += 1
+
+        self.spot_counter += 1 # move the spot up one
+        
+        if self.spot_counter == 4: # if there are four spots on the page
+            self.off_set -= 4 # change the offset to start on the next page
+            
+            self.l_n[1] += self.off_set
+            self.l_n[2] += self.off_set
+            self.l_n[3] += self.off_set
+            self.l_n[4] += self.off_set
+            self.l_n[5] += self.off_set
+            self.l_n[6] += self.off_set
+            self.l_n[7] += self.off_set
+            self.l_n[8] += self.off_set
+            self.l_n[9] += self.off_set
+            self.l_n[10] += self.off_set
+            self.l_n[11] += self.off_set
+            self.l_n[12] += self.off_set
+            self.l_n[13] += self.off_set
+            self.l_n[14] += self.off_set
+
 
             self.spot_counter = 0 # start counting from zero
             self.off_set += 4 # reset the offset and procede as normal
@@ -127,7 +213,14 @@ class Delivery_Slips():
             self.l_n[8] += self.off_set
             self.l_n[9] += self.off_set
             self.l_n[10] += self.off_set
-    
+            self.l_n[11] += self.off_set
+            self.l_n[12] += self.off_set
+            self.l_n[13] += self.off_set
+            self.l_n[14] += self.off_set
+
+
     def close_worksheet(self):
         self.workbook.close()
         print('workbook closed')
+
+
