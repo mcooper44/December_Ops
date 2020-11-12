d9f8beb6c65ab43c1ceb495a7499d4e91aab09c5
Matt Cooper <m.cooper44@gmail.com>
2020-11-12 13:10:46 -0500

    minor changes to print out more info

--- address_lookup.py
+++ address_lookup.py
@@ -1,13 +1,20 @@
 import googlemaps
 from r_config import configuration
 from address_parser_and_geocoder import GoogleResult
+import sys
+
 config = configuration.return_r_config()
 myapikey = config.get_g_creds()
 # INSTANTIATE GOOGLE MAPS CLIENT
 gmaps = googlemaps.Client(key=myapikey)
 
 if __name__ == '__main__':
-    while True:
+    go = True
+    while go:
         a = input('enter address\n')
-        response = gmaps.geocode(a)
-        result = GoogleResult(response[0])
+        if a:
+            response = gmaps.geocode(a)
+            result = GoogleResult(response[0])
+            print(result.result)
+        else:
+            sys.exit(0)
